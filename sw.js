dca57529c9e577ae8d5fed5355220ab3a011d3aa
kiryakovdk <59gm7ffggy@privaterelay.appleid.com>
2026-08-19 20:49:19 +0500

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,10 +1,4 @@
-const CACHE="salary-tracker-v2";
-const ASSETS=["./","./index.html","./manifest.webmanifest","./icon-180.png","./icon-512.png"];
-self.addEventListener("install",e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
+const CACHE="salary-tracker-v1";
+self.addEventListener("install",e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(["./","./index.html","./manifest.webmanifest"])).then(()=>self.skipWaiting())));
 self.addEventListener("activate",e=>e.waitUntil(self.clients.claim()));
-self.addEventListener("fetch",e=>{
- if(e.request.method!=="GET") return;
- e.respondWith(fetch(e.request).then(r=>{
-   const copy=r.clone(); caches.open(CACHE).then(c=>c.put(e.request,copy)); return r;
- }).catch(()=>caches.match(e.request)));
-});
+self.addEventListener("fetch",e=>{if(e.request.method==="GET")e.respondWith(fetch(e.request).then(r=>{const c=r.clone();caches.open(CACHE).then(x=>x.put(e.request,c));return r}).catch(()=>caches.match(e.request)))});
